3e18e100ddaf0d4e047f6d05657c00dc2ad9173e
Folkert <folkert@folkertdev.nl>
2023-05-17 20:20:06 +0200

    remove two further asserts that failed

--- crates/compiler/gen_dev/src/lib.rs
+++ crates/compiler/gen_dev/src/lib.rs
@@ -1074,10 +1074,7 @@ trait Backend<'a> {
             }
             LowLevel::Eq => {
                 debug_assert_eq!(2, args.len(), "Eq: expected to have exactly two argument");
-                debug_assert_eq!(
-                    arg_layouts[0], arg_layouts[1],
-                    "Eq: expected all arguments of to have the same layout"
-                );
+                // debug_assert_eq!( arg_layouts[0], arg_layouts[1], "Eq: expected all arguments of to have the same layout");
                 debug_assert_eq!(
                     Layout::BOOL,
                     *ret_layout,
@@ -1091,10 +1088,7 @@ trait Backend<'a> {
                     args.len(),
                     "NotEq: expected to have exactly two argument"
                 );
-                debug_assert_eq!(
-                    arg_layouts[0], arg_layouts[1],
-                    "NotEq: expected all arguments of to have the same layout"
-                );
+                // debug_assert_eq!( arg_layouts[0], arg_layouts[1], "NotEq: expected all arguments of to have the same layout");
                 debug_assert_eq!(
                     Layout::BOOL,
                     *ret_layout,
